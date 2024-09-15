a780057f76048160f45d4e4e7b7929561b412893
harsh00410 <harsh4781.be23@chitkara.edu.in>
2024-09-15 14:41:52 +0530

    Add files via upload

---
+++ Articleform.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ArticleForm = () => (
+    <div className="article-body">
+        <section className="article-abstract">
+            <h4>Abstract</h4>
+            <textarea 
+                placeholder="Abstract" 
+            />
+        </section>
+        <section className="article-text">
+            <h4>Article Text</h4>
+            <textarea 
+                placeholder="Enter the article content here..." 
+            />
+        </section>
+    </div>
+);
+
+export default ArticleForm;
---
+++ Questionform.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const QuestionForm = ({ title = "Describe your problem", placeholder = "Enter your question here..." }) => (
+    <div className="question-body">
+        <h4>{title}</h4>
+        <textarea 
+            type="text" 
+            name="describe"
+            placeholder={placeholder}
+        />
+    </div>
+);
+
+export default QuestionForm;
